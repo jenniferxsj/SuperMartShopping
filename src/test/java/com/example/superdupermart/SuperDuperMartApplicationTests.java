86ae55c9d9ac21ba362e42aadae0a9eab3952d1e
Shujun Xiao <xiao.57578@gmail.com>
2023-06-21 17:31:32 -0700

    First commit

---
+++ src/test/java/com/example/superdupermart/SuperDuperMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superdupermart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDuperMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
